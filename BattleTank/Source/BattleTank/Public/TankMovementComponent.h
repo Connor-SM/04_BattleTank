7ada3b55f907eb84787250fc9f0c622f3a8a9616
Connor Milliken <connor@sportsmogul.com>
2017-11-13 22:10:29 -0500

    Lecture 159 - A Better Component Architecture
    Instead of making the movement component a c++ added inherited class on the tank, we made it accessible to the blueprint, and set its' values through the beginplay funciton.

--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -5,18 +5,31 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+// Forward declaration
+class UTankTrack;
+
 /**
- * Class used for left thumbstick movement on tank "fly-by-controlls".
+ * Responsible for driving the tank tracks.
  */
-UCLASS(meta = (BlueprintSpawnableComponent))
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 {
 	GENERATED_BODY()
 
 public:
+	UFUNCTION(BlueprintCallable, Category = "Setup")
+	void Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
+
 	// Used for movement with left thumbstick on controller
 	UFUNCTION(BlueprintCallable, Category = "Input")
 	void IntendMoveForward(float Throw);
 	
-	
+private:
+	UTankTrack* LeftTrack = nullptr;
+	UTankTrack* RightTrack = nullptr;
+
+	// Max force per track, in newtons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; // Assume 40 ton tank, and 1g accelleration
+
 };
